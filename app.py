7adfb19dc4b829da97ca3344f8c7f91ce32e8941
Nicolas Paraskevas <nickrock126@gmail.com>
2020-08-12 00:01:12 +0300

    replaces on_message event with commands

--- app.py
+++ app.py
@@ -8,29 +8,27 @@ import cat
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
-
+client = commands.Bot(command_prefix = '!')
 catbot = cat.Cat()
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def meow(ctx):
+    await ctx.send(catbot.meow_back())
 
-    if message.content.startswith("!meow"):
-        await message.channel.send(catbot.meow_back())
+@client.command()
+async def catfact(ctx):
+    await ctx.send("{} Cat facts coming soon".format(ctx.message.author.mention))
 
-    if message.content.startswith("!catpic"):
-        resp = requests.get('https://api.thecatapi.com/v1/images/search')
-        if resp.status_code == 200:
-            for item in resp.json():
-                await message.channel.send(item['url'])
-    
-    if message.content.startswith("!catfact"):
-        await message.channel.send("@" + message.author.name + " Cat facts coming soon")
+@client.command()
+async def catpic(ctx):
+    resp = requests.get('https://api.thecatapi.com/v1/images/search')
+    if resp.status_code == 200:
+        for item in resp.json():
+            await ctx.send(item['url'])
+
 
 client.run(token)
